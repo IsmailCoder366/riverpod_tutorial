c55ab4ecf19b5dcbc5bcfaba3a04ca83f268b72f
IsmailCoder366 <ismailcoder366@gmail.com>
2026-09-23 21:52:42 +0500

    state provider screen style enhanced

--- lib/state_provider.dart
+++ lib/state_provider.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
-class StateProviderExample extends StatelessWidget {
+
+final counter = StateProvider<int>((ref){
+  return 0;
+});
+
+
+class StateProviderExample extends ConsumerWidget {
   const StateProviderExample({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final count = ref.watch(counter);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('State Provider Example'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(count.toString(), style: TextStyle(fontSize: 35),),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ButtonStyle(
+                  foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+                  backgroundColor: WidgetStateProperty.all<Color>(Colors.blue)
+                ),
+
+                  onPressed: (){}, child: Text('+')),
+              SizedBox(width: 50),
+              ElevatedButton(
+                  style: ButtonStyle(
+                      foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+                      backgroundColor: WidgetStateProperty.all<Color>(Colors.blue)
+                  ),
+
+                  onPressed: (){}, child: Text('-')),
+            ],
+          )
+        ],
+      )
+    );
   }
 }
