3040ccd4e9c9a4f5c191b580fc6f964aae5d8fdf
IsmailCoder366 <ismailcoder366@gmail.com>
2026-09-23 21:33:20 +0500

    simple provider exampled completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,25 +4,52 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
  /// Data Just to Read
-
 final hello = Provider<String>((ref) {
   return 'hello ismail';
 });
 
-
-class HomeScreen extends ConsumerWidget {
+final age = Provider<int>((ref){
+  return 24;
+});
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
     final greet = ref.watch(hello);
+    final myAge = ref.watch(age);
     return Scaffold(
       appBar: AppBar(
         title: Text('Riverpod Example'),
       ),
       body: Center(
-        child: Text(greet.toString()),
+        child: Text(greet + ' I am  ' + myAge.toString(), style: TextStyle(fontSize: 35),),
       ),
     );
   }
 }
+
+//
+// class HomeScreen extends ConsumerWidget {
+//   const HomeScreen({super.key});
+//
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final greet = ref.watch(hello);
+//     final myAge = ref.watch(age);
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Riverpod Example'),
+//       ),
+//       body: Center(
+//         child: Text(greet + ' I am  ' + myAge.toString(), style: TextStyle(fontSize: 35),),
+//       ),
+//     );
+//   }
+// }
