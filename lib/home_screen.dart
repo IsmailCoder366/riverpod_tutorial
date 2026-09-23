a3fe76c672bcf68ce1a439966c4a53cb91412a41
IsmailCoder366 <ismailcoder366@gmail.com>
2026-09-23 19:17:00 +0500

    appbar and center widget added

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Riverpod Example'),
+      ),
+      body: Center(
+        child: Text('Hello, Riverpod!'),
+      ),
+    );
   }
 }
