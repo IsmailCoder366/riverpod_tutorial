967eb63a7e0c2a5c95296e7730eabe384bf84634
IsmailCoder366 <ismailcoder366@gmail.com>
2026-09-24 15:06:40 +0500

    memory leaded escaped

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,31 +3,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_tutorial/slider_provider.dart';
 
-
-
-
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-  final sliderValue = ref.watch(sliderProvider);
+    print('built');
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Riverpod Example'),
-      ),
+      appBar: AppBar(title: Text('Riverpod Example')),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            height: 200,
-            width: 200,
-            color: Colors.red.withOpacity(sliderValue),
+          Consumer(
+
+            builder: (context, ref, child){
+              final sliderValue = ref.watch(sliderProvider);
+
+              print('build1');
+              return Container(
+                height: 200,
+                width: 200,
+                color: Colors.red.withOpacity(sliderValue),
+              );
+            }
           ),
-          Slider(value: sliderValue, onChanged: (value){
-ref.read(sliderProvider.notifier).state = value;
-          })
-          
+          Consumer(
+
+              builder: (context, ref, child){
+                final sliderValue = ref.watch(sliderProvider);
+
+                print('build');
+                return Slider(
+                  value: ref.watch(sliderProvider),
+                  onChanged: (value) {
+                    ref.read(sliderProvider.notifier).state = value;
+                  },
+                );
+              }
+          ),
+
         ],
       ),
     );
